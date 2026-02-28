d8d7b23a761fb58027c50e36139e399975689a30
chunghanliou@gmail.com <chunghanliou@gmail.com>
2026-02-27 21:53:10 -0500

    Fix: don't catch API routes in static file catch-all

--- server/vite.ts
+++ server/vite.ts
@@ -95,7 +95,12 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  // BUT don't catch API routes - those should 404 properly
+  app.use("*", (req, res, next) => {
+    if (req.originalUrl.startsWith('/api/')) {
+      // Let API routes 404 instead of returning HTML
+      return next();
+    }
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
